15497a1728899f7ff4c46791209a71ddc0036cfe
Natalic2020 <65713606+Natalic2020@users.noreply.github.com>
2020-12-17 15:41:46 +0100

    add once more test

--- src/test/java/ua/com/foxminded/dao/ScheduleItemDaoImplTest.java
+++ src/test/java/ua/com/foxminded/dao/ScheduleItemDaoImplTest.java
@@ -1,9 +1,11 @@
 package ua.com.foxminded.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -22,7 +24,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 import ua.com.foxminded.dao.entity.Group;
 import ua.com.foxminded.dao.entity.Room;
 import ua.com.foxminded.dao.entity.ScheduleItem;
@@ -31,53 +32,87 @@ import ua.com.foxminded.dao.entity.Subject;
 import ua.com.foxminded.dao.entity.Teacher;
 import ua.com.foxminded.dao.entity.TimeSlot;
 import ua.com.foxminded.dao.interfaces.ScheduleItemDao;
+import ua.com.foxminded.dao.interfaces.StudentDao;
 import ua.com.foxminded.util.FileParser;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 class ScheduleItemDaoImplTest {
 
-    private String scheduleItemUUID = "8f240794-fb1e-11ea-adc1-0242ac120002";
-    private String groupUUID = "0c149265-57c0-4942-a1e5-06c8b6983a23";
-    private String subjectUUID = "c4320591-215a-4b11-958e-9864c184ec2a";
-    private String roomUUID = "026621cc-73a6-40ba-8ea7-86628f4cb802";
-    private String timeSlotUUID = "d789bb56-b534-402b-8baa-38547218761c";
-    private String dayOfWeek = "MONDAY";
-    private String teacherUUID = "69c97b95-8ce5-4923-8d39-4c17bd5389d0";
-    private ScheduleItem scheduleItem;
+    private static String scheduleItemUUID = "8f240794-fb1e-11ea-adc1-0242ac120002";
+    private static String groupUUID = "0c149265-57c0-4942-a1e5-06c8b6983a23";
+    private static String subjectUUID = "c4320591-215a-4b11-958e-9864c184ec2a";
+    private static String roomUUID = "026621cc-73a6-40ba-8ea7-86628f4cb802";
+    private static String timeSlotUUID = "d789bb56-b534-402b-8baa-38547218761c";
+    private static String dayOfWeek = "MONDAY";
+    private static String teacherUUID = "69c97b95-8ce5-4923-8d39-4c17bd5389d0";
+
+    private static ScheduleItem scheduleItem;
     private ScheduleItem scheduleItemExpected;
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    FileParser file;
+    private ScheduleItemDao scheduleItemDao;
 
     @Autowired
     private TestEntityManager entityManager;
-    
-//    @Autowired
-//    @Qualifier("scheduleItemDao")
-//    ScheduleItemDao scheduleItemDao;
 
     @BeforeAll
-    void setUpBeforeClass() throws Exception {
-        jdbcTemplate.batchUpdate(file.readFileToLines("sql_test.script"));
-        jdbcTemplate.batchUpdate(file.readFileToLines("tables_test.script"));
-        scheduleItem = new ScheduleItem()
-                                         .setId(scheduleItemUUID)
-                                         .setGroup(new Group().setId(groupUUID))
-                                         .setSubject(new Subject().setId(subjectUUID))
-                                         .setRoom(new Room().setId(roomUUID))
-                                         .setTimeSlot(new TimeSlot().setId(timeSlotUUID))
-                                         .setDayOfWeek(dayOfWeek)
-                                         .setTeacher((Teacher) new Teacher().setIdPerson(teacherUUID));
+    static void Init() throws Exception {
 
-        scheduleItemExpected = new ScheduleItem().setDayOfWeek("MONDAY")
-                                                 .setGroup(new Group().setName("gr-1"))
-                                                 .setRoom(new Room().setName("room 1"))
-                                                 .setSubject(new Subject().setName("Maths"))
-                                                 .setTimeSlot(new TimeSlot().setSerialNumber(1));
+       scheduleItem = new ScheduleItem()
+                                                      .setId(scheduleItemUUID)
+                                                      .setGroup(new Group().setId(groupUUID))
+                                                      .setSubject(new Subject().setId(subjectUUID))
+                                                      .setRoom(new Room().setId(roomUUID))
+                                                      .setTimeSlot(new TimeSlot().setId(timeSlotUUID))
+                                                      .setDayOfWeek(dayOfWeek)
+                                                      .setTeacher((Teacher) new Teacher().setIdPerson(teacherUUID));
 
+        ScheduleItem scheduleItemExpected = new ScheduleItem().setDayOfWeek("MONDAY")
+                                                              .setGroup(new Group().setName("gr-1"))
+                                                              .setRoom(new Room().setName("room 1"))
+                                                              .setSubject(new Subject().setName("Maths"))
+                                                              .setTimeSlot(new TimeSlot().setSerialNumber(1));
+
+    }
+
+    @Test
+    public void scheduleFindByTeacherIdPerson() {
+        List<ScheduleItem> schedule = scheduleItemDao.findByTeacherIdPerson(teacherUUID);
+        assertThat(schedule.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void scheduleFindByGroupIdGroup() {
+        List<ScheduleItem> schedule = scheduleItemDao.findByGroupIdGroup(groupUUID);
+        assertThat(schedule.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void scheduleDeleteById() {
+        scheduleItemDao.deleteById("5e9330ba-162f-45ea-b9de-605ae734f585");
+        List<ScheduleItem> schedule = scheduleItemDao.findByTeacherIdPerson(teacherUUID);
+        assertThat(schedule.size()).isEqualTo(2);
+    }
+    
+    @Test
+    public void scheduleDeleteById_uuidFalse() {
+        scheduleItemDao.deleteById("cb0aba06-3f97-11eb-b378-0242ac130002");
+        List<ScheduleItem> schedule = scheduleItemDao.findByTeacherIdPerson(teacherUUID);
+        assertThat(schedule.size()).isEqualTo(2);
+    }
+    
+    @Test
+    public void scheduleSave_addNew_sizePlusOne() {
+        scheduleItemDao.save(scheduleItem);
+        List<ScheduleItem> schedule = scheduleItemDao.findByTeacherIdPerson(teacherUUID);
+        assertThat(schedule.size()).isEqualTo(4);
+    }
+   
+    @Test
+    public void scheduleSave_editOldSchedule_sizeTheSame() {
+        scheduleItemDao.save(scheduleItem.setId("5e9330ba-162f-45ea-b9de-605ae734f585"));
+        List<ScheduleItem> schedule = scheduleItemDao.findByTeacherIdPerson(teacherUUID);
+        assertThat(schedule.size()).isEqualTo(3);
     }
 }
